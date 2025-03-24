b6759048dda3f877737ac72bdc828cde699e1b69
lemon <jackson.w.lam@gmail.com>
2025-03-24 13:45:36 -0500

    Fixed wall jump pt.2

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
     private static readonly int IsRunning = Animator.StringToHash("isRunning");
     private static readonly int IsJumping = Animator.StringToHash("isJumping");
     private static readonly int IsClimbing = Animator.StringToHash("isClimbing");
+    
+    [Header("Movement settings")]
     public float speed = 5f;
     public float jumpForce = 7f;
     private bool _isGrounded;
@@ -17,11 +20,14 @@ public class PlayerMovement : MonoBehaviour
     private float _climbSpeed;
     private float _wallJumpForce;
     private float _jumpHeight;
-    private float _wallJumpTime = 0.5f;  // Time window to allow wall jumping
-    private bool _canWallJump;
-    private float _lastWallTouchTime;
+    
+    
     private float _facingDirection = 1f; // 1 for right, -1 for left
     private bool _isTouchingWall;
+    
+    // Coyote time
+    public float wallJumpTime = 0.5f;  // Time window to allow wall jumping
+    private float _wallJumpTimer;
     
     private SpriteRenderer _sr;
     private Rigidbody _rb;
@@ -58,6 +64,9 @@ public class PlayerMovement : MonoBehaviour
         _anim.SetBool(IsJumping, _isJumping);
         _anim.SetBool(IsClimbing, _onWall);
 
+        // Horizontal movement
+        _rb.linearVelocity = new Vector3(_move * speed, _rb.linearVelocity.y, 0);
+        
         // Jumping
         if (Input.GetButtonDown("Jump") && (_isGrounded || _onWall))
         {
@@ -76,18 +85,12 @@ public class PlayerMovement : MonoBehaviour
         {
             _sr.flipY = false;
         }
-    }
-    void FixedUpdate()
-    {
-        // Horizontal movement
-        _rb.linearVelocity = new Vector3(_move * speed, _rb.linearVelocity.y, 0);
         
+        // Climbing
         _isTouchingWall = IsTouchingWall();
-
         if (_isTouchingWall)
         {
-            _canWallJump = true;
-            _lastWallTouchTime = _wallJumpTime;
+            _wallJumpTimer = wallJumpTime;
             if (Input.GetKey(KeyCode.W)) // Climb up with W
             {
                 WallClimb(1f); // Move Up
@@ -103,18 +106,21 @@ public class PlayerMovement : MonoBehaviour
                 // If no input, stop vertical movement
                 _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0, 0);
             }
-            if (Input.GetKeyDown(KeyCode.Space) && (_canWallJump) && (_lastWallTouchTime > 0))
-            {
-                WallJump();
-            }
         }
         else
         {
             _rb.useGravity = true;
-            _lastWallTouchTime -= Time.deltaTime;
+            _wallJumpTimer -= Time.deltaTime;
+            if(_wallJumpTimer <= 0)
+                _wallJumpTimer = 0;
         }
         
-
+        if (Input.GetKeyDown(KeyCode.Space) && (_wallJumpTimer > 0))
+        {
+            Debug.Log("Space is pressed in the air");
+            WallJump();
+        }
+        
         // Handle wall flip direction
         if (Input.GetAxis("Horizontal") > 0) // Moving right
         {
@@ -144,8 +150,9 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector3 jumpDirection = transform.localScale.x > 0 ? Vector3.left : Vector3.right;
             _rb.linearVelocity = new Vector3(jumpDirection.x * _wallJumpForce, _jumpHeight, 0);
-            _canWallJump = false; // Reset the ability to wall jump
         }
+        
+        Debug.Log(_wallJumpTimer);
     }
 
     private void OnCollisionEnter(Collision collision)
